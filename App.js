eec23c5f4539fc9da534082d2094393c721a08df
조인식 <imsif42@naver.com>
2024-11-28 19:20:09 +0900

    feat: layout

--- App.js
+++ App.js
@@ -7,28 +7,36 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
-      <View style={{ margin: 12, borderWidth: 2, borderColor: 'black' }}>
+      <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
-        <Text>12</Text>
       </View>
-      <Button title="hihi" color={'red'} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
